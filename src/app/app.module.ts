199524ca333fd7b4963c3a2e616aa851097d12ee
Chet Hill <chetdhill@gmail.com>
2018-08-05 10:10:24 -0400

    Push app to Github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterNavComponent } from './footer-nav/footer-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterNavComponent
   ],
   imports: [
     BrowserModule
